2137ce4bb0869f9914309e2e72f63428f86c670b
ismaildevv@gmail.com <ismaildevv@gmail.com>
2023-02-17 17:33:33 +0100

    Added thank you page

--- interactive-rating-component-main/app.js
+++ interactive-rating-component-main/app.js
@@ -2,4 +2,7 @@ let selectedRating=1
 
 document.getElementById('rating-submit').addEventListener('click',()=>{
     selectedRating = document.querySelector('.radio-container input:checked').getAttribute("data-rating")
+    document.getElementById("user-rating").innerText = selectedRating
+    document.querySelector(".rating-component .rating-page").classList.add("hidden")
+    document.querySelector(".rating-component .thanks-page").classList.remove("hidden")
 })
