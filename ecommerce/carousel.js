945736308c61449c04a225b6e8b15d4bc145e591
ismail <ismaildevv@gmail.com>
2024-01-27 19:17:02 +0100

    added lightbox

--- ecommerce/carousel.js
+++ ecommerce/carousel.js
@@ -1,8 +1,11 @@
+const lightbox = document.querySelector(".lightbox");
 const imageContainer = document.getElementById('imageContainer');
+const lightboxImageContainer = document.getElementById('lightboxImageContainer');
 const thumbnails = document.querySelectorAll('.carousel img');
 const nextButton = document.querySelector('.next');
 const previousButton = document.querySelector('.previous');
-
+const lightboxNextButton = document.querySelector('.lightbox .next');
+const lightboxPreviousButton = document.querySelector('.lightbox .previous');
 
 const updateImage = (index) => {
     const selectedThumbnail = thumbnails[index];
@@ -10,22 +13,54 @@ const updateImage = (index) => {
     selectedThumbnail.classList.add('selected');
     const imagePath = selectedThumbnail.src.replace('-thumbnail', '');
     imageContainer.src = imagePath;
+    lightboxImageContainer.src = imagePath;
+};
+
+const handleThumbnailClick = (index) => {
+    updateImage(index);
+};
+
+const handleArrowClick = (direction) => {
+    const currentIndex = Array.from(thumbnails).findIndex(thumb => thumb.classList.contains('selected'));
+    const newIndex = (currentIndex + direction + thumbnails.length) % thumbnails.length;
+    updateImage(newIndex);
+};
+
+const handleLightboxClick = (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.add('hidden');
+    }
 };
 
 thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        updateImage(index);
+    thumbnail.addEventListener('click', (e) => {
+        e.stopPropagation();
+        handleThumbnailClick(index);
     });
 });
 
-nextButton.addEventListener('click', () => {
-    const currentIndex = Array.from(thumbnails).findIndex(thumb => thumb.classList.contains('selected'));
-    const nextIndex = (currentIndex + 1) % thumbnails.length;
-    updateImage(nextIndex);
+nextButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    handleArrowClick(1);
 });
 
-previousButton.addEventListener('click', () => {
-    const currentIndex = Array.from(thumbnails).findIndex(thumb => thumb.classList.contains('selected'));
-    const previousIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
-    updateImage(previousIndex);
+previousButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    handleArrowClick(-1);
+});
+
+lightboxNextButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    handleArrowClick(1);
+});
+
+lightboxPreviousButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    handleArrowClick(-1);
+});
+
+lightbox.addEventListener('click', handleLightboxClick);
+
+imageContainer.addEventListener('click', () => {
+    lightbox.classList.remove("hidden");
 });
